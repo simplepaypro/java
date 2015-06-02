324d38d9f583073302844acf62d3cd0b6f36ee81
SimplePay <simplepaypro@users.noreply.github.com>
2015-06-02 14:18:23 +0300

    Example with HTTP GET query generation

--- com/simplepay/SP_Java_Example.java
+++ com/simplepay/SP_Java_Example.java
@@ -8,13 +8,14 @@ package com.simplepay;
 public class SP_Java_Example {
 
     public static void main(String[] args) throws Exception{
-        SP_Signature signature = new SP_Signature("YOUR_SECRET_KEY","payment");
-        signature.set_param("sp_outlet_id","ID_ТОРГОВОЙ_ТОЧКИ");
-        signature.set_param("sp_description", "Тестовый заказ из Java");
-        signature.set_param("sp_amount","10.00");
 
-        System.out.println("Concatenation string: "+signature.make_concat());
-        System.out.println("Signature string: "+signature.make_signature());
+        SP_Query query = new SP_Query("https://api.simplepay.pro/sp/payment", "1AlQ2f3Aa2bM9sHaL0JaF6BaE5Un12m3xY0WT1R4");
+        query.setParam("sp_outlet_id", "2");
+        query.setParam("sp_description", "Test from JAVA");
+        query.setParam("sp_amount", "1000.50");
+
+        String query_url = query.makeQueryString();
+        System.out.println("Query URL: "+query_url);
 
     }
 }
